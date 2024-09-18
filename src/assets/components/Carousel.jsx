2a96291f2c0652214bfd4a09be98a11886b44a87
mostafa400 <code8015@gmail.com>
2024-09-18 22:27:31 +0300

    adjustements

--- src/assets/components/Carousel.jsx
+++ src/assets/components/Carousel.jsx
@@ -1,12 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { IoArrowForwardCircle, IoArrowBackCircleSharp } from "react-icons/io5";
-import { useState } from "react";
-import combindData from "../../Data/Data.js";
 import { Link } from "react-router-dom";
+
+const carouselData = [
+  {
+    image: "public/images/wireless-01.png",
+    headline: "50% off your first purchase",
+    paragraph:
+      "All you have to do is simply log in to your account and you'll get 50% off your first shipping for free, no matter where you are!",
+    button: "Shop Now",
+  },
+  {
+    image: "public/images/watch-06.png",
+    headline: "Smart Watches for You",
+    paragraph:
+      "Stay connected and track your fitness with our latest collection of cutting-edge smartwatches. Enhance your lifestyle today!",
+    button: "Shop Now",
+  },
+  {
+    image: "public/images/single-sofa-07.png",
+    headline: "Experience the Future",
+    paragraph:
+      "Discover our range of innovative gadgets designed to make your life easier, more efficient, and more enjoyable.",
+    button: "Shop Now",
+  },
+  {
+    image: "public/images/phone-08.png",
+    headline: "Unlock the Power of Mobile",
+    paragraph:
+      "Explore our selection of state-of-the-art smartphones with advanced features to keep you connected and productive on the go.",
+    button: "Shop Now",
+  },
+];
 
 function Carousel() {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const Data = combindData.mainData;
+
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
@@ -16,22 +45,22 @@ function Carousel() {
 
   const nextSlide = () => {
     setCurrentSlide((prevSlide) =>
-      prevSlide === Data.length - 1 ? 0 : prevSlide + 1
+      prevSlide === carouselData.length - 1 ? 0 : prevSlide + 1
     );
   };
 
   const prevSlide = () => {
     setCurrentSlide((prevSlide) =>
-      prevSlide === 0 ? Data.length - 1 : prevSlide - 1
+      prevSlide === 0 ? carouselData.length - 1 : prevSlide - 1
     );
   };
 
-  const currentData = Data[currentSlide];
+  const currentData = carouselData[currentSlide];
 
   return (
-    <div className=" lg:h-[560px] md:h-[420px] h-screen  w-full mt-10 md:flex-row flex  flex-col items-center relative bg-slate-100  group overflow-hidden ">
-      <div className="lg:w-1/2 lg:p-20 lg:ml-60  md:w-1/2 md:ml-20 ml-5 md:text-left text-center pt-20 ">
-        <h1 className="lg:text-6xl md:text-4xl text-3xl font-bold ">
+    <div className="lg:h-[560px] md:h-[420px] h-screen w-full mt-10 md:flex-row flex flex-col items-center relative bg-slate-100 group overflow-hidden">
+      <div className="lg:w-1/2 lg:p-20 lg:ml-60 md:w-1/2 md:ml-20 ml-5 md:text-left text-center pt-20">
+        <h1 className="lg:text-6xl md:text-4xl text-3xl font-bold">
           {currentData.headline}
         </h1>
         <p className="lg-text-2xl md:text-2xl md:p-0 p-3 md:mt-6 mt-3 text-xl">
@@ -44,19 +73,19 @@ function Carousel() {
         </Link>
       </div>
       <div
-        style={{ backgroundImage: `url(${currentData.image.src})` }}
-        className="lg:w-1/4 lg:h-full md:w-1/4 md:h-full w-1/2 h-full rounded-2xl bg-center bg-cover duration-500  "
+        style={{ backgroundImage: `url(${currentData.image})` }}
+        className="lg:w-1/4 lg:h-full md:w-1/4 md:h-full w-1/2 h-full rounded-2xl bg-center bg-cover duration-500"
       ></div>
       <div>
         <IoArrowForwardCircle
           onClick={nextSlide}
-          className=" hidden group-hover:block text-4xl  absolute lg:right-10 md:right-8 sm:right-4 right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+          className="hidden group-hover:block text-4xl absolute lg:right-10 md:right-8 sm:right-4 right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
         />
       </div>
       <div>
         <IoArrowBackCircleSharp
           onClick={prevSlide}
-          className=" hidden group-hover:block text-4xl absolute lg:left-10 md:left-8 sm:left-4 left-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+          className="hidden group-hover:block text-4xl absolute lg:left-10 md:left-8 sm:left-4 left-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
         />
       </div>
     </div>
